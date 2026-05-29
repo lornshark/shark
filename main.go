7ddb68d0d8117733109393d57407ca34a0690ce1
kent <917628331@qq.com>
2026-05-29 15:44:02 +0800

    优化

--- main.go
+++ main.go
@@ -1,19 +1,7 @@
 package main
 
 import (
-	"context"
-	"fmt"
-
 	"github.com/lornshark/shark/sharkapp"
-	"github.com/lornshark/shark/sharkdb"
-	"github.com/lornshark/shark/sharkelastic"
-	"github.com/lornshark/shark/sharkjson"
-	"github.com/lornshark/shark/sharkkafka"
-	"github.com/lornshark/shark/sharkmongodb"
-	"github.com/lornshark/shark/sharkrabbitmq"
-	"github.com/lornshark/shark/sharkredis"
-	"github.com/lornshark/shark/sharkrisingwave"
-	"github.com/spf13/cast"
 )
 
 type MyApp struct {
@@ -21,33 +9,12 @@ type MyApp struct {
 }
 
 func main() {
-	ctx := context.Background()
-	configRedis, err := sharkredis.New(ctx, &sharkredis.Config{
-		Host:     "192.168.191.100",
-		Port:     6379,
-		Password: "CEki57pxTJyYaLD",
-	})
-	if err != nil {
-		panic(err)
-	}
 	project := "kgame"
 	name := "game-test"
 	id := "1"
-	options := sharkapp.NewOption("dev", project, name, id)
-	options.WithDb(sharkjson.ParseJsonString[sharkdb.Config](configRedis.Get(ctx, project+":system:config:db").Val()))
-	options.WithRedis(sharkjson.ParseJsonString[sharkredis.Config](configRedis.Get(ctx, project+":system:config:redis").Val()))
-	options.WithKafka(sharkjson.ParseJsonString[sharkkafka.Config](configRedis.Get(ctx, project+":system:config:kafka").Val()))
-	options.WithElastic(sharkjson.ParseJsonString[sharkelastic.Config](configRedis.Get(ctx, project+":system:config:elastic").Val()))
-	options.WithMongodb(sharkjson.ParseJsonString[sharkmongodb.Config](configRedis.Get(ctx, project+":system:config:mongodb").Val()))
-	options.WithRabbitmq(sharkjson.ParseJsonString[sharkrabbitmq.Config](configRedis.Get(ctx, project+":system:config:rabbitmq").Val()))
-	options.WithRisingwave(sharkjson.ParseJsonString[sharkrisingwave.Config](configRedis.Get(ctx, project+":system:config:risingwave").Val()))
-	projectConfig := sharkjson.ParseJsonString[map[string]any](configRedis.Get(ctx, fmt.Sprintf("%v:system:config:%v-%v", project, name, id)).Val())
-	options.WithTimer(cast.ToBool((*projectConfig)["timer"]))
-	options.WithHttp(cast.ToInt((*projectConfig)["http"]))
-	options.WithGrpc(cast.ToInt((*projectConfig)["grpc"]))
-	options.WithPprof(cast.ToInt((*projectConfig)["pprof"]))
-	options.WithHealth(cast.ToInt((*projectConfig)["health"]))
-
+	options := sharkapp.NewOptionWithRedis("dev", project, name, id, "192.168.191.100", "6379", "CEki57pxTJyYaLD")
+	options.WithDb(nil).WithRedis(nil).WithKafka(nil).WithElastic(nil).
+		WithMongodb(nil).WithRabbitmq(nil).WithRisingwave(nil)
 	app, err := sharkapp.New(options)
 	if err != nil {
 		panic(err)
--- sharkapp/option.go
+++ sharkapp/option.go
@@ -1,17 +1,26 @@
 package sharkapp
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/lornshark/shark/sharkdb"
 	"github.com/lornshark/shark/sharkelastic"
+	"github.com/lornshark/shark/sharkjson"
 	"github.com/lornshark/shark/sharkkafka"
 	"github.com/lornshark/shark/sharkminio"
 	"github.com/lornshark/shark/sharkmongodb"
 	"github.com/lornshark/shark/sharkrabbitmq"
 	"github.com/lornshark/shark/sharkredis"
 	"github.com/lornshark/shark/sharkrisingwave"
+	"github.com/spf13/cast"
 )
 
 type Options struct {
+	config_redis_host     string
+	config_redis_port     string
+	config_redis_password string
+
 	id         string
 	db         *sharkdb.Config
 	env        string
@@ -40,43 +49,164 @@ func NewOption(env, project, name, id string) *Options {
 	}
 }
 
-func (s *Options) WithKafka(kafka *sharkkafka.Config) *Options {
-	s.kafka = kafka
+func (s *Options) WithKafka(config *sharkkafka.Config) *Options {
+	if config == nil {
+		r, err := sharkredis.New(context.Background(), &sharkredis.Config{
+			Host:     s.config_redis_host,
+			Port:     cast.ToInt(s.config_redis_port),
+			Password: s.config_redis_password,
+		})
+		if err != nil {
+			return s
+		}
+		value, err := r.Get(context.Background(), s.project+":system:config:kafka").Result()
+		if err != nil {
+			return s
+		}
+		config = sharkjson.ParseJsonString[sharkkafka.Config](value)
+	}
+	s.kafka = config
 	return s
 }
 
-func (s *Options) WithRedis(redis *sharkredis.Config) *Options {
-	s.redis = redis
+func (s *Options) WithRedis(config *sharkredis.Config) *Options {
+	if config == nil {
+		r, err := sharkredis.New(context.Background(), &sharkredis.Config{
+			Host:     s.config_redis_host,
+			Port:     cast.ToInt(s.config_redis_port),
+			Password: s.config_redis_password,
+		})
+		if err != nil {
+			return s
+		}
+		value, err := r.Get(context.Background(), s.project+":system:config:redis").Result()
+		if err != nil {
+			return s
+		}
+		config = sharkjson.ParseJsonString[sharkredis.Config](value)
+	}
+	s.redis = config
 	return s
 }
 
 func (s *Options) WithDb(config *sharkdb.Config) *Options {
+	if config == nil {
+		r, err := sharkredis.New(context.Background(), &sharkredis.Config{
+			Host:     s.config_redis_host,
+			Port:     cast.ToInt(s.config_redis_port),
+			Password: s.config_redis_password,
+		})
+		if err != nil {
+			return s
+		}
+		value, err := r.Get(context.Background(), s.project+":system:config:db").Result()
+		if err != nil {
+			return s
+		}
+		config = sharkjson.ParseJsonString[sharkdb.Config](value)
+	}
 	s.db = config
 	return s
 }
 
-func (s *Options) WithElastic(elastic *sharkelastic.Config) *Options {
-	s.elastic = elastic
+func (s *Options) WithElastic(config *sharkelastic.Config) *Options {
+	if config == nil {
+		r, err := sharkredis.New(context.Background(), &sharkredis.Config{
+			Host:     s.config_redis_host,
+			Port:     cast.ToInt(s.config_redis_port),
+			Password: s.config_redis_password,
+		})
+		if err != nil {
+			return s
+		}
+		value, err := r.Get(context.Background(), s.project+":system:config:elastic").Result()
+		if err != nil {
+			return s
+		}
+		config = sharkjson.ParseJsonString[sharkelastic.Config](value)
+	}
+	s.elastic = config
 	return s
 }
 
-func (s *Options) WithRabbitmq(rabbitmq *sharkrabbitmq.Config) *Options {
-	s.rabbitmq = rabbitmq
+func (s *Options) WithRabbitmq(config *sharkrabbitmq.Config) *Options {
+	if config == nil {
+		r, err := sharkredis.New(context.Background(), &sharkredis.Config{
+			Host:     s.config_redis_host,
+			Port:     cast.ToInt(s.config_redis_port),
+			Password: s.config_redis_password,
+		})
+		if err != nil {
+			return s
+		}
+		value, err := r.Get(context.Background(), s.project+":system:config:rabbitmq").Result()
+		if err != nil {
+			return s
+		}
+		config = sharkjson.ParseJsonString[sharkrabbitmq.Config](value)
+	}
+	s.rabbitmq = config
 	return s
 }
 
-func (s *Options) WithRisingwave(risingwave *sharkrisingwave.Config) *Options {
-	s.risingwave = risingwave
+func (s *Options) WithRisingwave(config *sharkrisingwave.Config) *Options {
+	if config == nil {
+		r, err := sharkredis.New(context.Background(), &sharkredis.Config{
+			Host:     s.config_redis_host,
+			Port:     cast.ToInt(s.config_redis_port),
+			Password: s.config_redis_password,
+		})
+		if err != nil {
+			return s
+		}
+		value, err := r.Get(context.Background(), s.project+":system:config:risingwave").Result()
+		if err != nil {
+			return s
+		}
+		config = sharkjson.ParseJsonString[sharkrisingwave.Config](value)
+	}
+	s.risingwave = config
 	return s
 }
 
-func (s *Options) WithMongodb(mongodb *sharkmongodb.Config) *Options {
-	s.mongodb = mongodb
+func (s *Options) WithMongodb(config *sharkmongodb.Config) *Options {
+	if config == nil {
+		r, err := sharkredis.New(context.Background(), &sharkredis.Config{
+			Host:     s.config_redis_host,
+			Port:     cast.ToInt(s.config_redis_port),
+			Password: s.config_redis_password,
+		})
+		if err != nil {
+			return s
+		}
+		value, err := r.Get(context.Background(), s.project+":system:config:mongodb").Result()
+		if err != nil {
+			return s
+		}
+		config = sharkjson.ParseJsonString[sharkmongodb.Config](value)
+	}
+	s.mongodb = config
 	return s
 }
 
-func (s *Options) WithMinio(minio *sharkminio.Config) *Options {
-	s.minio = minio
+func (s *Options) WithMinio(config *sharkminio.Config) *Options {
+	if config == nil {
+		r, err := sharkredis.New(context.Background(), &sharkredis.Config{
+			Host:     s.config_redis_host,
+			Port:     cast.ToInt(s.config_redis_port),
+			Password: s.config_redis_password,
+		})
+		if err != nil {
+			return s
+		}
+		value, err := r.Get(context.Background(), s.project+":system:config:minio").Result()
+		if err != nil {
+			return s
+		}
+		config = sharkjson.ParseJsonString[sharkminio.Config](value)
+	}
+
+	s.minio = config
 	return s
 }
 
@@ -103,4 +233,33 @@ func (s *Options) WithGrpc(port int) *Options {
 func (s *Options) WithHttp(port int) *Options {
 	s.http = port
 	return s
+}
+
+func NewOptionWithRedis(env, project, name, id, host, port, password string) *Options {
+	options := NewOption(env, project, name, id)
+	options.config_redis_host = host
+	options.config_redis_port = port
+	options.config_redis_password = password
+	k := fmt.Sprintf("%v:system:config:%v-%v", project, name, id)
+	r, err := sharkredis.New(context.Background(), &sharkredis.Config{
+		Host:     host,
+		Port:     cast.ToInt(port),
+		Password: password,
+	})
+	if err != nil {
+		return options
+	}
+	value, err := r.Get(context.Background(), k).Result()
+	if err != nil {
+		return options
+	}
+	mvalue := sharkjson.ParseJsonString[map[string]any](value)
+	if mvalue != nil {
+		options.WithTimer(cast.ToBool((*mvalue)["timer"]))
+		options.WithHttp(cast.ToInt((*mvalue)["http"]))
+		options.WithGrpc(cast.ToInt((*mvalue)["grpc"]))
+		options.WithPprof(cast.ToInt((*mvalue)["pprof"]))
+		options.WithHealth(cast.ToInt((*mvalue)["health"]))
+	}
+	return options
 }
