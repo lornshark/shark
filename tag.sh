60d9167ca62afa2dc77155cf9475c014dcc3ecf0
kent <917628331@qq.com>
2026-06-04 11:21:26 +0800

    commit

--- tag.sh
+++ tag.sh
@@ -10,7 +10,7 @@ if [ -z "$LATEST_TAG" ]; then
     exit 1
 fi
 
-echo "当前最新 Tag: $LATEST_TAG"
+echo "当前 Tag: $LATEST_TAG"
 
 # 提取最后一段版本号
 PREFIX=$(echo "$LATEST_TAG" | sed -E 's/\.[0-9]+$//')
@@ -19,7 +19,7 @@ LAST_NUM=$(echo "$LATEST_TAG" | sed -E 's/^.*\.([0-9]+)$/\1/')
 NEW_NUM=$((LAST_NUM + 1))
 NEW_TAG="${PREFIX}.${NEW_NUM}"
 
-echo "新 Tag: $NEW_TAG"
+echo "新建 Tag: $NEW_TAG"
 
 # 创建 tag
 git tag "$NEW_TAG"
